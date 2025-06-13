a06bf858a9536b510325c7ff94120c1613916fa6
GKravale <gunakravale@gmail.com>
2025-06-13 18:55:09 +0300

    not done Course class

--- src/main/java/lv/venta/model/Course.java
+++ src/main/java/lv/venta/model/Course.java
@@ -1,5 +1,49 @@
 package lv.venta.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lv.venta.model.enums.CourseLevel;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@Table(name = "Course_Table")
+@Entity
 public class Course {
+	
+	// Nosaukums, stundas, limenis
+	
+	@Setter(value = AccessLevel.NONE)
+	@Id
+	@Column(name = "CId")
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int cId;
+	
+	@NotNull
+	@Column(name = "Title")
+	@Size(min = 3, max = 30)
+	private String title;
+	
+	@NotNull
+	@Column(name = "Level")
+	private CourseLevel courseLevel;
+	
+	
+	// pasniedzejs?
+	
+	
+	
 
 }
