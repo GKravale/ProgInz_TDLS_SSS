3dc7c343936561698a463d8647a9284ea618d279
martHaza <martazapacka17@gmail.com>
2025-06-14 22:23:48 +0300

    Iesākts course serviss

--- src/main/java/lv/venta/service/ICourseService.java
+++ src/main/java/lv/venta/service/ICourseService.java
@@ -1,7 +1,19 @@
 package lv.venta.service;
 
-public interface ICourseService {
+import java.util.ArrayList;
 
+import lv.venta.model.Course;
+
+public interface ICourseService {
 	
+	public abstract ArrayList<Course> selectAllCourses() throws Exception;
+	
+	public abstract Course selectCourseByID(int cId) throws Exception;
+	
+	public abstract void deleteCourseByID(int cId) throws Exception;
+	
+	public abstract void insertNewCourse(Course course) throws Exception;
+	
+	public abstract void updateCourseByID(int cId, Course course) throws Exception;
 	
 }
