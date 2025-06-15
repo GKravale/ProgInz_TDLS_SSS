4ef5b8775057fc8f2585e0cd982ed5a311d867b1
GKravale <gunakravale@gmail.com>
2025-06-16 01:57:37 +0300

    Update ICourseRepo.java

--- src/main/java/lv/venta/repo/ICourseRepo.java
+++ src/main/java/lv/venta/repo/ICourseRepo.java
@@ -2,9 +2,6 @@ package lv.venta.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import org.springframework.data.repository.CrudRepository;
-
-
 import lv.venta.model.Course;
 
 public interface ICourseRepo extends JpaRepository<Course, Integer> {
