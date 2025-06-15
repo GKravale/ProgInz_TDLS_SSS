73c8c7fe24fe6a88f1f34e901daed5baa7d99b67
martHaza <martazapacka17@gmail.com>
2025-06-15 15:02:56 +0300

    add-page and course add controller

--- src/main/java/lv/venta/controller/CourseController.java
+++ src/main/java/lv/venta/controller/CourseController.java
@@ -1,5 +1,7 @@
 package lv.venta.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import lv.venta.model.Course;
 import lv.venta.service.ICourseService;
 
 @Controller
@@ -15,9 +18,9 @@ public class CourseController {
 
 	@Autowired
 	private ICourseService courseService;
-	
+
 	// get /course/crud/show/all
-	@GetMapping("/all") //localhost:8080/course/crud/all
+	@GetMapping("/all") // localhost:8080/course/crud/all
 	public String getAllCourses(org.springframework.ui.Model model) {
 		try {
 			model.addAttribute("package", courseService.selectAllCourses());
@@ -27,12 +30,11 @@ public class CourseController {
 			return "error-page";
 		}
 	}
-	
+
 	// get /course/crud/show/all/{id}
 	@GetMapping("/all/{cid}") // localhost":8080/course/crud/all/1
 	public String getAllCoursesById(@PathVariable("cid") int cId, Model model) {
 		try {
-				
 			model.addAttribute("package", courseService.selectCourseById(cId));
 			return "course-one-page";
 		} catch (Exception e) {
@@ -40,5 +42,26 @@ public class CourseController {
 			return "error-page";
 		}
 	}
+
+	// get /course/crud/remove/{id}
+	@GetMapping("/remove/{did}") // localhost:8080/course/crud/remove/2
+	public String getCourseRemove(@PathVariable("cid") int cId, Model model) {
+		try {
+			courseService.deleteCourseById(cId);
+			ArrayList<Course> allCourses = courseService.selectAllCourses();
+			model.addAttribute("package", allCourses);
+			return "doctor-all-page";
+		} catch (Exception e) {
+			model.addAttribute("package", e.getMessage());
+			return "error-page";
+		}
+	}
 	
+	@GetMapping("/add") // localhost:8080/course/crud/add
+	public String getCourseAdd(Model model) {
+		model.addAttribute("course", new Course());
+		return "course-add-page";
+	}
+	
+
 }
