e467da70eac4ee88b36f5b1b87bb52b2260ec73c
martHaza <martazapacka17@gmail.com>
2025-06-15 17:48:30 +0300

    template all page and controller

--- src/main/java/lv/venta/controller/TemplateController.java
+++ src/main/java/lv/venta/controller/TemplateController.java
@@ -1,5 +1,32 @@
 package lv.venta.controller;
 
-public class TemplateController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import lv.venta.service.ITemplateService;
+
+@Controller
+@RequestMapping("/template/crud")
+public class TemplateController {
+	
+
+		@Autowired
+		private ITemplateService templateService;
+		
+		@GetMapping("/all") // localhost:8080/template/crud/all
+		public String getAllTemplates(Model model) {
+			try {
+				model.addAttribute("package", templateService.selectAllTemplates());
+				return "template-all-page";
+			} catch (Exception e) {
+				model.addAttribute("package", e.getMessage());
+				return "error-page";
+			}
+		}
+		
+		
+	
 }
