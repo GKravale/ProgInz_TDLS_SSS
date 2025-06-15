e35fac97fe4b71e424f9e2bcadaecb75d2630bb0
martHaza <martazapacka17@gmail.com>
2025-06-15 14:45:42 +0300

    course one page un getById course

--- src/main/java/lv/venta/controller/CourseController.java
+++ src/main/java/lv/venta/controller/CourseController.java
@@ -2,7 +2,9 @@ package lv.venta.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lv.venta.service.ICourseService;
@@ -20,6 +22,19 @@ public class CourseController {
 		try {
 			model.addAttribute("package", courseService.selectAllCourses());
 			return "course-all-page";
+		} catch (Exception e) {
+			model.addAttribute("package", e.getMessage());
+			return "error-page";
+		}
+	}
+	
+	// get /course/crud/show/all/{id}
+	@GetMapping("/all/{cid}") // localhost":8080/course/crud/all/1
+	public String getAllCoursesById(@PathVariable("cid") int cId, Model model) {
+		try {
+				
+			model.addAttribute("package", courseService.selectCourseById(cId));
+			return "course-one-page";
 		} catch (Exception e) {
 			model.addAttribute("package", e.getMessage());
 			return "error-page";
