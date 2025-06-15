6ad791ede16112ac72a8bad35fa216fc1b4f46d9
martHaza <martazapacka17@gmail.com>
2025-06-15 14:02:36 +0300

    Iesākts course impl

--- src/main/java/lv/venta/service/Impl/CourseServiceImpl.java
+++ src/main/java/lv/venta/service/Impl/CourseServiceImpl.java
@@ -1,5 +1,73 @@
 package lv.venta.service.Impl;
 
-public class CourseServiceImpl {
+import java.util.ArrayList;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import lv.venta.model.Course;
+import lv.venta.repo.ICourseRepo;
+import lv.venta.service.ICourseService;
+
+@Service
+public class CourseServiceImpl implements ICourseService {
+
+	@Autowired
+	private ICourseRepo courseRepo;
+	
+	@Override
+	public ArrayList<Course> selectAllCourses() throws Exception {
+		if(courseRepo.count() == 0) {
+			throw new Exception("courseRepo ir tukša!");
+		}
+		return (ArrayList<Course>) courseRepo.findAll();
+	}
+
+	@Override
+	public Course selectCourseByID(int cId) throws Exception {
+		if(cId < 0) {
+			throw new Exception("Kursa ID nevar būt negatīvs!");
+		}
+		
+		if(!courseRepo.existsById(cId)) {
+			throw new Exception("Kurss ar tādu ID nepastāv!");
+		}
+		
+		return courseRepo.findById(cId).get();
+	}
+
+	@Override
+	public void deleteCourseByID(int cId) throws Exception {
+		if (cId < 0) {
+            throw new Exception("Kusa ID jābūt pozitīvam!");
+        }
+        // 
+		
+	}
+
+	@Override
+	public void insertNewCourse(Course course) throws Exception {
+		if(courseRepo.existsById(course.getCId())) {
+			throw new Exception("Kurss ar tādu ID jau eksistē!");
+		}
+		
+		courseRepo.save(course);
+		
+	}
+
+	@Override
+	public void updateCourseByID(int cId, Course course) throws Exception {
+		Course update = selectCourseByID(cId);
+		update.setTitle(course.getTitle());
+		update.setDescription(course.getDescription());
+		update.setHours(course.getHours());
+		update.setCourseLevel(course.getCourseLevel());
+		update.setLecturer(course.getLecturer());
+		update.setCourseDates(course.getCourseDates());
+		
+		courseRepo.save(update);
+		
+	}
+
+	
 }
